005946f7497c6d190507c341b59a9335600bbc05
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-11-10 11:23:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ else:
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME'.herokuapp.com'
 URL = "https://madarabot-9a205e6fff5e.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://madarabot-9a205e6fff5e.herokuapp.com/".format(FQDN, PORT)
+    "https://madarabot-9a205e6fff5e/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
