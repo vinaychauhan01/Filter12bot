d08084127a67c79f2948fd97a9030e886f3a7246
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-11-10 11:28:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME'.herokuapp.com'
-URL = "https://madarabot-9a205e6fff5e.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+URL = "https://madarabot-9a205e6fff5e".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://madarabot-9a205e6fff5e/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
