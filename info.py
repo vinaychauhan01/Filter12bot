c08cc9d91380420733b09a933b551ef1510a9f33
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-11-11 08:58:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS',
-'https://telegra.ph/file/0af58e51a879c940dccd1.jpg
+
+PICS = environ.get('PICS','https://telegra.ph/file/0af58e51a879c940dccd1.jpg
 https://telegra.ph/file/8b123c01484dc3ef27472.jpg
 https://telegra.ph/file/823b6af6191910af6a8e5.jpg
 https://telegra.ph/file/8af31419679b087e4f668.jpg
